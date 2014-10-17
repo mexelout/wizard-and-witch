92db247798507239228b2c4b1d61323bd8167311
杉山元和 <mexelout@mexelout.local>
2014-10-17 18:45:12 +0900

    複数プレイヤーをWebsocketで同期処理

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ WizardAndWitch::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root "field#show"
+  root "field#show"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
